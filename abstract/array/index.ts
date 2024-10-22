03e9550a72cc1c264c1bd31fe3561ed1d0b0f1a3
Paul Jack <134734881+p-jack@users.noreply.github.com>
2024-10-22 15:37:02 -0700

    0.0.1

---
+++ abstract/array/index.ts
@@ -0,0 +1,15 @@
+import { DataStructure, RODataStructure } from "loudo-ds-core"
+
+export const array = Symbol("array")
+
+export interface ROArray<T> extends RODataStructure<T,number> {
+  [array]:true
+  at(i:number):T
+}
+
+export interface Array<T> extends DataStructure<T>, ROArray<T> {
+  set(i:number,v:T):void
+  add(v:T,i?:number):void
+  addAll(v:Iterable<T>,i?:number):void
+  remove(i?:number):void
+}
