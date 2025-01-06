ffa1a7ad977be6951da91de67f6cfc505081b101
Paul Jack <134734881+p-jack@users.noreply.github.com>
2025-01-05 17:03:44 -0800

    0.0.8

--- abstract/mapsort/index.ts
+++ abstract/mapsort/index.ts
@@ -1,6 +1,6 @@
 import { mixin } from "loudo-mixin"
 import { BaseMap, Entry } from "loudo-ds-map-interfaces"
-import { Include, Sized, Stash } from "loudo-ds-core"
+import { Include, Loud, Sized, Stash } from "loudo-ds-core"
 
 export abstract class BaseMapSort<K extends {},V extends {}> {
   abstract get compare():(a:K,b:K)=>number
@@ -18,5 +18,5 @@ mixin(BaseMapSort, [BaseMap])
 export abstract class MapSortChange<K extends {},V extends {}> {
   abstract set compare(cmp:(a:K,b:K)=>number)
 }
-export interface MapSortChange<K extends {},V extends {}> extends BaseMapSort<K,V> {}
+export interface MapSortChange<K extends {},V extends {}> extends BaseMapSort<K,V>, Loud<Entry<K,V>> {}
 mixin(MapSortChange, [BaseMapSort])
